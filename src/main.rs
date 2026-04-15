90ec79b75c7942a1482e74952849652d442d35ee
Will McDonald <wemcdonald@gmail.com>
2026-04-14 21:58:50 -0700

    fix: default daemon log level to info instead of silent

--- src/main.rs
+++ src/main.rs
@@ -172,7 +172,10 @@ fn main() -> anyhow::Result<()> {
         Commands::Daemon { action } => match action {
             DaemonAction::Run { once } => {
                 tracing_subscriber::fmt()
-                    .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+                    .with_env_filter(
+                        tracing_subscriber::EnvFilter::try_from_default_env()
+                            .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+                    )
                     .with_writer(std::io::stderr)
                     .init();
                 let config = disk_inventory::config::Config::load()?;
